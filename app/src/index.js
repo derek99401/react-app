e3a1605637fc9a15f8659bb675f9138d485224e5
derek99401 <derek99401@gmail.com>
2017-09-06 10:04:56 -0700

    reset author

---
+++ app/src/index.js
@@ -0,0 +1,12 @@
+/*
+ * ./src/index.js
+ * webpack entry file
+ */
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+
+ReactDOM.render(
+  <App />, document.getElementById('root')
+)
---
+++ app/webpack.config.js
@@ -0,0 +1,31 @@
+/*
+ * Webpack configuration
+ */
+
+const path = require('path')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const htmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index_bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+  plugins: [htmlWebpackPluginConfig]
+}
+
